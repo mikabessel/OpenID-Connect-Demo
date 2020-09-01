886807094b19a80d348156b20784039bf83bef5b
Mika Bessel <m.bessel@inspired.ag>
2020-09-01 14:59:41 +0200

    With okta

--- src/main/java/inspired/OpenIDConnectDemo/controller/HelloController.java
+++ src/main/java/inspired/OpenIDConnectDemo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package inspired.OpenIDConnectDemo.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String index(@AuthenticationPrincipal OidcUser user) {
+        return "Welcome, " + user.getFullName();
     }
 
     @GetMapping("/restricted")
