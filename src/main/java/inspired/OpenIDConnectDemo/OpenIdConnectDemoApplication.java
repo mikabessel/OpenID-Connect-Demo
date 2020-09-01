68fbf3d230c58ba298deda63018fac22f11f3a4d
Mika Bessel <m.bessel@inspired.ag>
2020-09-01 10:44:33 +0200

    Initial commit

---
+++ src/main/java/inspired/OpenIDConnectDemo/OpenIdConnectDemoApplication.java
@@ -0,0 +1,13 @@
+package inspired.OpenIDConnectDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenIdConnectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenIdConnectDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inspired/OpenIDConnectDemo/OpenIdConnectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package inspired.OpenIDConnectDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenIdConnectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
